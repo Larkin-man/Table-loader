8a61cca647ab73d505579fc18c663078bac37ed1
Larkin <ab88199594ae@mail.ru>
2022-11-03 17:42:08 +0300

    create 1.7
    String
    RegColumn

--- TableLoader.h
+++ TableLoader.h
@@ -8,60 +8,66 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 /******************************************************************\
-* TableLoader - Загрузка таблиц из текстовых файлов. Версия 1.6    *
-* Unicode, LoadFromResource                                        *
+* TableLoader - Загрузка таблиц из текстовых файлов. Версия 1.7    *
+* + typedef char lrchar                                            *
 \******************************************************************/
 #ifndef TableLoaderH
 #define TableLoaderH
-
 typedef char lrchar;
+struct TLSection
+{
+	String *Name;
+	int Pos;	//Позиция секции с 0
+	int Size;//Кол-во строк
+};
 
 class TableLoader
 {
 private:
    char *FFormat;
 	//Mem обязательно [Count][FRowCount]
-   UnicodeString **MemStr;
+	String **MemStr;
    int **MemInt;
 	lrchar **MemChar;
    bool **MemBool;
 	int StrCount, IntCount, CharCount, BoolCount;
    int FRowCount;
 	int FColCount;
-	void AddRowToSection(UnicodeString SectionName, UnicodeString text, int Pos);
 
 protected:    
-	struct Section
-	{
-		UnicodeString *Name;
-      int Pos; //Позиция секции с 0
-		int Size;//Кол-во строк
-   } *FSections;
+	TLSection *FSections;
    void SectionEnsureCapacity(int count);
    int SectionCapacity;
 	int FSectionCount;
+	void Load(TStringList *list, const char *format);
+	int RealGetSection(int SectionIdx, va_list &args);
 
 public:     
-   __fastcall TableLoader();
+	__fastcall TableLoader();
 	__property char* Format = {read = FFormat};
 	__property int RowCount = {read = FRowCount};
-   __property int ColCount = {read = FColCount};
+	__property int ColCount = {read = FColCount};
 	__property int SectionCount = {read = FSectionCount};
-	__property Section* Sections = {read = FSections};
-	Section* FindSection(UnicodeString SectionName);
+	__property TLSection* Sections = {read = FSections};
+	TLSection* FindSection(String SectionName);
 	bool IgnoreFirstString; //Игнорировать первую сроку в файле. defaul=true
 	bool IgnoreDelimitersPack; //Игнорировать разделители, идущие подряд. defauly=true
 	lrchar Delimiter;      //Разделитель столбцов (табуляция)
-	//Загрузка из файла, format: i-int c-lrchar s-Unicode b-bool, ... список ссылок на переменные
-	int Load(UnicodeString Filename, bool FileOrResource, const char *format, ...);
-	//GetSection: ... это список ссылок, также как в LoadFromFile. Возвращает кол-во строк в секции
-	int GetSection(const UnicodeString SectionName, ...);
+   String EndMark; 		//"[end]"
+	//Загрузка, format: i-int c-lrchar s-String b-bool, ... список ссылок на переменные
+	int LoadFromFile(String Filename, const char *format, ...);
+	int LoadFromResource(String ResourceName, const char *format, ...);
+	int LoadFromList(TStrings *list, const char *format, ...);
+	//GetSection: ... это список ссылок, также как в LoadFrom. Возвращает кол-во строк в секции
+	int GetSection(const String SectionName="", ...);
 	//RegColumn - устанавливает ссылку на столбец в секции. ColNum - Номер столбца соотв типа. Возвращает кол-во строк в секции
-	int RegColumn(int* &Field, int ColNum, const UnicodeString SectionName = "");
-	int RegColumn(lrchar* &Field, int ColNum, const UnicodeString SectionName = "");
-	int RegColumn(UnicodeString* &Field, int ColNum, const UnicodeString SectionName = "");
-	int RegColumn(bool* &Field, int ColNum, const UnicodeString SectionName = "");
-	void GetCount(int &IntCount, int &CharCount, int &BoolCount, int &StrCount);	void __fastcall Clear();
-	__fastcall ~TableLoader();
+	int RegColumn(int* &Field, int ColNum, const String SectionName = "") const;
+	int RegColumn(lrchar* &Field, int ColNum, const String SectionName = "") const;
+	int RegColumn(String* &Field, int ColNum, const String SectionName = "") const;
+	int RegColumn(bool* &Field, int ColNum, const String SectionName = "") const;
+	void GetCount(int &intCount, int &charCount, int &boolCount, int &strCount) const;
+	void Clear();
+	__fastcall ~TableLoader() { Clear(); };
 };
+
 #endif
